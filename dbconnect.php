8ce1402188aad613606e33dd89b9068af59431d5
LahiruSujith <lahirusujith9999@gmail.com>
2023-08-30 00:17:19 +0530

    database conmnect
    create dbconnect, log out and footer

---
+++ dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+    $servername='localhost';
+    $username='root';
+    $password='';
+    $dbname='sasidu';
+
+    $con=mysqli_connect($servername,$username,$password,$dbname) or die("DB Connection failed");
+?>
---
+++ footer.php
@@ -0,0 +1,5 @@
+        <footer class="bg-danger fixed-bottom">
+            <p class="text-center mt-2 fw-bold">Lahiru_S_Madusanka&copy;2023</p>
+        </footer>
+    </body>
+</html>
