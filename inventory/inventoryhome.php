5159712b96111849ed10f61697d8318b76adb02f
LahiruSujith <lahirusujith9999@gmail.com>
2023-09-03 21:55:54 +0530

    create Inventory
    Create inventory folder and create navbar and footer for inventory create inventory home page

---
+++ inventory/inventoryhome.php
@@ -0,0 +1,12 @@
+<?php include('./navbar.php'); ?>
+
+<div class="container mt-5 text-center">
+    <h1>Welcome to the Inventory Dashboard!</h1>
+    <p>This is the main content of inventory page.</p>
+    <a class="btn btn-primary" href="#">Go to Products Details</a>
+    <a class="btn btn-primary" href="#">Go to Metierials Details</a>
+
+  </div>
+
+
+<?php include('./footer.php'); ?>
